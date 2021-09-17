918692bed93249cc34be39880413496184391973
tttyyzzz <tohyizhi9@gmail.com>
2021-09-17 17:13:46 +0800

    stage hunk

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -32,8 +32,14 @@ public class Main {
         }
         return count;
     }
-
+    private static int countDeadlinesUsingStream(ArrayList<Task> tasks) {
+        int count = (int)tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .count();
+        return count;
+    }
     public static void printData(ArrayList<Task> tasksData) {
+        System.out.println("Printing data by looping");
         for (Task t : tasksData) {
             System.out.println(t);
         }
