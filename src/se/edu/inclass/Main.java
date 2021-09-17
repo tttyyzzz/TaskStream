6de7f7eddf345b9a1ed26586503fe34372253046
tttyyzzz <tohyizhi9@gmail.com>
2021-09-17 17:38:44 +0800

    Add operations using lambdas

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -20,7 +20,9 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
-
+        printDeadlinesUsingStream(tasksData);
+        ArrayList<Task> filteredList = filterTasksbyString(tasksData, "11");
+        printData(filteredList);
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -45,5 +47,17 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks){
+        tasks.stream()
+                .filter((t) -> t instanceof Deadline)
+                .sorted((a, b) -> a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
+                .forEach(System.out::println);
+    }
+    public static ArrayList<Task> filterTasksbyString(ArrayList<Task> tasks, String filterString) {
+        ArrayList<Task> filteredList = (ArrayList<Task>)tasks.stream()
+                .filter((t) -> t.getDescription().contains(filterString))
+                .collect(Collectors.toList());
+        return filteredList;
     }
 }
